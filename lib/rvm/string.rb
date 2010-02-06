460b078cfae8081275c7871bc3b9ab6455aff6d1
Christian Höltje <docwhat@gerf.org>
2010-02-05 23:22:24 -0500

    cleaned up the string.

--- lib/rvm/string.rb
+++ lib/rvm/string.rb
@@ -1,8 +1,10 @@
 # Monkey patch String to add a starts_with? method.
 class String
-  def starts_with?(prefix)
-    prefix = prefix.to_s
-    self[0, prefix.length] == prefix
+  if not method_defined? "starts_with?"
+    def starts_with?(prefix)
+      prefix = prefix.to_s
+      self[0, prefix.length] == prefix
+    end
   end
 end
 
---
+++ spec/rvm/string_spec.rb
@@ -0,0 +1,14 @@
+
+require 'rvm'
+
+describe String do
+  it "should correctly detect starting strings" do
+    "foo bar".starts_with?("foo").should be_true
+    "Mousing Cat".starts_with?("Mousin").should be_true
+  end
+
+  it "should correctly fail to detect incorrect starting strings" do
+    "foo bar".starts_with?("Foo").should be_false
+    "foo bar".starts_with?("bar").should be_false
+  end
+end
